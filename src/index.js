4ba9b0d232cc3514b740d7974bd3918a45740c7e
kessjelly <jessicaskelly01@gmail.com>
2025-06-23 18:02:12 +1000

    Added basic Javascript and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateStory(event) {
+  event.preventDefault();
 
+  new Typewriter("#story", {
+    strings: "Winston is a fat cat",
+    autoStart: true,
+    Delay: 1,
+  });
+}
+
+let storyFormElement = document.querySelector("#story-generator-form");
+storyFormElement.addEventListener("submit", generateStory);
